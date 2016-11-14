e7fe916d47db84bddd5aa84548b29812504ee90b
osuisumi <osuisumi@163.com>
2016-11-14 09:37:52 +0800

    t

--- src/main/java/chapter0_2/App.java
+++ src/main/java/chapter0_2/App.java
@@ -2,7 +2,11 @@ package chapter0_2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -12,8 +16,33 @@ public class App {
 	 * 最小可用id的暴力解法
 	 */
 
-	// 假设当前的使用id情况是[18, 4, 8, 9, 16, 1, 14, 7, 19, 3, 0, 5, 2, 11, 6]
-	private static int[] ids = { 18, 4, 8, 9, 16, 1, 14, 7, 19, 3, 0, 5, 2, 11, 6 ,10,12,13};
+	// 假设当前的使用id情况是
+	private static Integer[] ids = {18, 4, 8, 9, 16, 1, 14, 7, 19, 3, 0, 5, 2, 11, 6};
+	
+	/*static{
+		Random r = new Random();
+		Set<Integer> set = new HashSet<>();
+		int max = 1000000;
+		for(int i=0;i<max;i++){
+			set.add(r.nextInt(max));
+		}
+		Iterator<Integer> it = set.iterator();
+		int i = 0;
+		List<Integer> idsList = new ArrayList<>();
+		while(it.hasNext()){
+			idsList.add(it.next());
+			i++;
+		}
+		ids = new Integer [idsList.size()];
+		int index = 0;
+		for(Integer id:idsList){
+			//System.out.println(id);
+			ids[index] = id;
+			index ++ ;
+		}
+		ids[0] = 0;
+		
+	}*/
 
 	/*
 	 * 时间复杂度O(n*n)
@@ -22,6 +51,7 @@ public class App {
 	public void simpleSulution() {
 		int i = 0;
 		boolean flg = true;
+		long begin = System.currentTimeMillis();
 		while (flg) {
 			for (int j = 0; j < ids.length; j++) {
 				if (ids[j] == i) {
@@ -35,7 +65,8 @@ public class App {
 
 			}
 		}
-
+		long end = System.currentTimeMillis();
+		System.out.println((end-begin)/1000);
 		System.out.println(i);
 	}
 
@@ -79,6 +110,47 @@ public class App {
 		System.out.println(ids.length);
 	}
 	
+	/*
+	 * 1234
+	 * 12 34
+	 * 3 4
+	 * 4
+	 * 
+	 */
+	
+	/*
+	 * 1235
+	 * 12 35
+	 * 35
+	 * 3 5
+	 * null 5
+	 * 
+	 */
+	
+	/*
+	 * 1356
+	 * 1 356
+	 */
+	/*
+	 * 135
+	 * 1 35
+	 * 
+	 */
+	
+	/*
+	 * 125
+	 * 12 5
+	 */
+	
+	/*
+	 * 697481
+	 */
+	
+	public int findmin(List<Integer> input){
+		//int size = 
+		return 0;
+	}
+	
 
 	public Integer findMin(List<Integer> input, Integer position,Integer size) {
 		List<Integer> smallH = new ArrayList<Integer>();
@@ -88,14 +160,14 @@ public class App {
 		}
 
 		for (int i = 0; i < input.size(); i++) {
-			if (input.get(i) < (size+position) / 2) {
+			if (input.get(i) <= (size+position) / 2) {
 				smallH.add(input.get(i));
 			} else {
 				bigH.add(input.get(i));
 			}
 		}
 		//double b = input.size() / 2.0;
-		if (smallH.size() < input.size() / 2) {
+		if (smallH.size() <= input.size() / 2) {
 			return findMin(smallH, position,position + input.size()/2);
 		}
 		return findMin(bigH, (size+position) / 2,size);
---
+++ src/main/java/chapter0_2/Test.java
@@ -0,0 +1,15 @@
+package chapter0_2;
+
+import java.util.Random;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		/*Random r = new Random();
+		for(int i=0;i<999;i++){
+			System.out.println(r.nextInt(999));
+		}*/
+		System.out.println(Math.floor((3+0l)/2));
+	}
+
+}
